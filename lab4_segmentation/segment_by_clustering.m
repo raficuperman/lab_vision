280df6d4ad748271fc5b392379790b2caf3b27d2
raficuperman <r.cuperman675@uniandes.edu.co>
2015-02-26 21:32:28 -0500

    Corrección función para segmentar

--- lab4_segmentation/segment_by_clustering.m
+++ lab4_segmentation/segment_by_clustering.m
@@ -1,16 +1,39 @@
-function [] = segment_by_clustering( rgb_image, feature_space, clustering_method, number_of_clusters)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+function [my_segmentation] = segment_by_clustering( rgb_image, feature_space, clustering_method, number_of_clusters)
+%segment_by_clustering Función para segmentar una imagen
+%   rgb_image es la imagen a segmentar
+%   feature_space es el espacio de representación. Puede ser ('rgb' por defecto):
+%       'rgb', 'lab', 'hsv', 'rgb+xy', 'lab+xy', 'hsv+xy' 
+%   clustering_method es el método de clustering. Puede ser ('kmeans' por defecto):
+%       'kmeans', 'gmm', 'hierarchical', 'watersheds'
+%   number_of_clusters es el número de clusters. Debe ser un número mayor o
+%       igual a 1. (2 por defecto)
 
 %% Inicialización
 close all
 clc
 addpath('images')
+if nargin < 2
+    feature_space='rgb';
+end
+if nargin < 3
+    clustering_method='kmeans';
+end
+if nargin < 4
+    number_of_clusters=2;
+end
+if (number_of_clusters<1)
+   error('Número de clusters inválido'); 
+end
 I=imread(rgb_image);
+
+%%  Reducción de tamaño de imagen para jerárquico
+if strcmp(clustering_method,'hierarchical')==1
+    I=imresize(I,0.5);
+end
+%% Tamaño de la imagen
+[M,N,~]=size(I);
 figure, imshow(I)
 title('Imagen original')
-[M,N,~]=size(I);
-
 %%  Conversión de colores
 
 switch feature_space
@@ -59,9 +82,11 @@ switch feature_space
               IReshape(temp,5)=j;
            end
         end
-    otherwise %rgb
+    case 'rgb'
         IReshape=reshape(I(:),[],3);
         IReshape=double(IReshape);
+    otherwise
+        error('Se introdujo un espacio de representación inválido');
 end
 
 %% Clustering
@@ -72,9 +97,15 @@ switch clustering_method
         clusters=reshape(clusters,M,N);
         figure,imshow(clusters,[])
         colormap colorcube
-        title('Clustering por gmm')
-
+        my_segmentation=clusters;
     case 'hierarchical'
+        Z = linkage(IReshape,'ward','euclidean','savememory','on');
+        clusters = cluster(Z,'maxclust',number_of_clusters);
+        clusters=reshape(clusters(:),M,N);
+        figure,imshow(clusters,[])
+        colormap colorcube
+        my_segmentation=clusters;
+    case 'watersheds'
         I=rgb2gray(I);
         hy= fspecial('sobel');
         hx= hy';
@@ -85,27 +116,19 @@ switch clustering_method
         new_grad= imimposemin(grad, marker);
         ws= watershed(new_grad);
         figure,imshow(ws==0)
-        %colormap colorcube
-        title('Clustering por hierarchical')
-    case 'watershed'
-        %gradient’smagnitude
-        hy= fspecial('sobel');
-        hx= hy';
-        Iy= imfilter(double(I), hy, 'replicate');
-        Ix = imfilter(double(I), hx, 'replicate');
-        grad = sqrt(Ix.^2 + Iy.^2);
-        % watersheds
-        ws= watershed(grad);
-        figure,imshow(ws==0)
-        %colormap colorcube
-        title('Clustering por watershed')
-    otherwise %kmeans
+        my_segmentation=new_grad;
+        
+    case 'kmeans'
         clusters=kmeans(IReshape, number_of_clusters);
         clusters=reshape(clusters,M,N);
         figure,imshow(clusters,[])
         colormap colorcube
-        title('Clustering por kmeans')
-
+        my_segmentation=clusters;
+    otherwise
+        error('Se introdujo un método de clustering inválido');
 end
+        title({['Clustering por ',clustering_method,...
+            ' bajo espacio de representación ',feature_space],...
+            ['con ', num2str(number_of_clusters), ' clusters']})
 end
 
